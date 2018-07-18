c03f924eb5d66bb4d02e624dfeee6f30461276a6
Damian Bushong <stratosphere.programming@gmail.com>
2018-07-18 15:42:58 -0500

    update copyright info

--- build/ExpandingResource.js
+++ build/ExpandingResource.js
@@ -2,7 +2,7 @@
 //
 // ByteAccordion - JS library for smooth, Promise-based interaction with File and Buffer resources.
 //
-// @copyright (c) 2017 Damian Bushong <katana@odios.us>
+// @copyright (c) 2018 Damian Bushong <katana@odios.us>
 // @license MIT license
 // @url <https://github.com/damianb/ByteAccordion>
 //
